41dd13cae957ac51ea1d298cb9cc7364533ba3a5
Manh Cuong <nguyenmanhcuong123nmc@gmail.com>
2025-10-22 20:06:51 +0700

    tao project

---
+++ src/main/java/com/project/it_job/ItJobApplication.java
@@ -0,0 +1,13 @@
+package com.project.it_job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItJobApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/project/it_job/service/imp/testServiceImp.java
@@ -0,0 +1,4 @@
+package com.project.it_job.service.imp;
+
+public class testServiceImp {
+}
