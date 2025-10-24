a047bc3bc6212a74cfc46e2a3bb19b6b933bf652
Tran Thanh Duy <thanhduy04022004@gmail.com>
2025-10-24 23:36:36 +0700

    fix ProvinceMapper và WardMapper từ interface sang class

--- src/main/java/com/project/it_job/mapper/WardMapper.java
+++ src/main/java/com/project/it_job/mapper/WardMapper.java
@@ -1,16 +1,38 @@
 package com.project.it_job.mapper;
 
 import com.project.it_job.dto.WardDTO;
+import com.project.it_job.entity.Province;
 import com.project.it_job.entity.Ward;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import org.springframework.stereotype.Component;
 
-@Mapper(componentModel = "spring")
-public interface WardMapper {
-    @Mapping(source = "province.id", target = "idProvince")
-    WardDTO toDTO(Ward ward);
+@Component
+public class WardMapper {
 
-    @Mapping(source = "idProvince", target = "province.id")
-    Ward toEntity(WardDTO dto);
+    public WardDTO toDTO(Ward entity) {
+        if (entity == null) return null;
+        WardDTO dto = new WardDTO();
+        dto.setId(entity.getId());
+        dto.setName(entity.getName());
 
+        if (entity.getProvince() != null) {
+            dto.setIdProvince(entity.getProvince().getId());
+        }
+
+        return dto;
+    }
+
+    public Ward toEntity(WardDTO dto) {
+        if (dto == null) return null;
+        Ward entity = new Ward();
+        entity.setId(dto.getId());
+        entity.setName(dto.getName());
+
+        if (dto.getIdProvince() > 0) {
+            Province province = new Province();
+            province.setId(dto.getIdProvince());
+            entity.setProvince(province);
+        }
+
+        return entity;
+    }
 }
