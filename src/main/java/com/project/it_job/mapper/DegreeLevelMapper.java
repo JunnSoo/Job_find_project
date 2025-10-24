efe4eceefd610ea3b14d25b93222821106ecbbfb
Tran Thanh Duy <thanhduy04022004@gmail.com>
2025-10-24 23:24:01 +0700

    sửa đổi file DegreeLevelMapper từ interface sang class

--- src/main/java/com/project/it_job/mapper/DegreeLevelMapper.java
+++ src/main/java/com/project/it_job/mapper/DegreeLevelMapper.java
@@ -2,10 +2,24 @@ package com.project.it_job.mapper;
 
 import com.project.it_job.dto.DegreeLevelDTO;
 import com.project.it_job.entity.DegreeLevel;
-import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 
-@Mapper(componentModel = "spring")
-public interface DegreeLevelMapper {
-    DegreeLevelDTO toDTO(DegreeLevel degreeLevel);
-    DegreeLevel toEntity(DegreeLevelDTO dto);
+@Component
+public class DegreeLevelMapper {
+
+    public DegreeLevelDTO toDTO(DegreeLevel entity) {
+        if (entity == null) return null;
+        DegreeLevelDTO dto = new DegreeLevelDTO();
+        dto.setId(entity.getId());
+        dto.setName(entity.getName());
+        return dto;
+    }
+
+    public DegreeLevel toEntity(DegreeLevelDTO dto) {
+        if (dto == null) return null;
+        DegreeLevel entity = new DegreeLevel();
+        entity.setId(dto.getId());
+        entity.setName(dto.getName());
+        return entity;
+    }
 }
