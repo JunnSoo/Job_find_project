19ec9ac10cf65cf4a1060c5d02231cd8da137a6d
HuynhBao <huynhbao0626@gmail.com>
2025-10-26 14:54:08 +0700

    review Commit

--- src/main/java/com/project/it_job/controller/auth/UserController.java
+++ src/main/java/com/project/it_job/controller/auth/UserController.java
@@ -1,4 +1,4 @@
-package com.project.it_job.controller;
+package com.project.it_job.controller.auth;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
