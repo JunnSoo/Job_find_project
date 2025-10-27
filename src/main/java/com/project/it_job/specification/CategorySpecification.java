2ca4b43b6607d4a4693d96dc6a21ae7d064e838a
Phu Nguyen <trungphuntp@gmail.com>
2025-10-27 15:48:23 +0700

    feature search category

---
+++ src/main/java/com/project/it_job/specification/CategorySpecification.java
@@ -0,0 +1,16 @@
+package com.project.it_job.specification;
+
+import com.project.it_job.entity.Blog;
+import org.springframework.data.jpa.domain.Specification;
+
+public class CategorySepecification {
+    public static Specification<Blog> searchByName(String keyword) {
+        if (keyword == null || keyword.isEmpty()) return null; // neu trong tra null
+        String pattern = "%" + keyword + "%"; //pattern
+        return (root, query, cb) -> cb.like(root.get("name").as(String.class), pattern );
+    }
+
+    public static Specification<Blog> parentIsNull() {
+        return (root, query, cb) -> cb.isNull(root.get("parent"));
+    }
+}
