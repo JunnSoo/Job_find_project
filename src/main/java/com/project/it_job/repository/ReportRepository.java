396945369028d880a99cec375a6a48ca2ffa8a87
Tran Thanh Duy <thanhduy04022004@gmail.com>
2025-10-27 23:31:22 +0700

    lam phan trang cho bang report

--- src/main/java/com/project/it_job/repository/ReportRepository.java
+++ src/main/java/com/project/it_job/repository/ReportRepository.java
@@ -2,8 +2,9 @@ package com.project.it_job.repository;
 
 import com.project.it_job.entity.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ReportRepository extends JpaRepository<Report, Integer> {
+public interface ReportRepository extends JpaRepository<Report, Integer>, JpaSpecificationExecutor<Report> {
 }
