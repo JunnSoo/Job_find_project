3ef0f1cf00912fd3e39ff3713881d7de300ece0e
HuynhBao <huynhbao0626@gmail.com>
2025-10-29 02:36:46 +0700

    CRUD-CV-USER

--- src/main/java/com/project/it_job/repository/auth/UserRepository.java
+++ src/main/java/com/project/it_job/repository/auth/UserRepository.java
@@ -10,6 +10,4 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, String>, JpaSpecificationExecutor<User> {
     Optional<User> findByEmail(String email);
-
-    User findUserByIdIgnoreCase(String id);
 }
