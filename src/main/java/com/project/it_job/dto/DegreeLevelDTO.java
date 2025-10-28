ae85a06a1664c7da26b38a535c994fa319f3ae6a
Tran Thanh Duy <thanhduy04022004@gmail.com>
2025-10-28 19:33:49 +0700

    Hoàn thành CRUD của bảng degreeLevel

--- src/main/java/com/project/it_job/dto/DegreeLevelDTO.java
+++ src/main/java/com/project/it_job/dto/DegreeLevelDTO.java
@@ -1,8 +1,12 @@
 package com.project.it_job.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class DegreeLevelDTO {
     private int id;
     private String name;
---
+++ src/main/java/com/project/it_job/request/DegreeLevelRequest.java
@@ -0,0 +1,8 @@
+package com.project.it_job.request;
+
+import lombok.Data;
+
+@Data
+public class DegreeLevelRequest {
+    private String name;
+}
