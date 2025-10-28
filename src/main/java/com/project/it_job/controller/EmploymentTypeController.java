a7c1f715482ad222fb21956d0a19ec28570b8c8f
Tran Thanh Duy <thanhduy04022004@gmail.com>
2025-10-28 19:57:00 +0700

    Hoàn thành tính năng CRUD cho bảng employmentType

--- src/main/java/com/project/it_job/controller/EmploymentTypeController.java
+++ src/main/java/com/project/it_job/controller/EmploymentTypeController.java
@@ -1,6 +1,7 @@
 package com.project.it_job.controller;
 
 import com.project.it_job.entity.EmploymentType;
+import com.project.it_job.request.EmploymentTypeRequest;
 import com.project.it_job.response.BaseResponse;
 import com.project.it_job.service.EmploymentTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,71 +20,29 @@ public class EmploymentTypeController {
     private EmploymentTypeService employmentTypeService;
 
     @GetMapping
-    public ResponseEntity<BaseResponse> getAll(){
-        List<EmploymentType> list = employmentTypeService.getAll();
-        BaseResponse response = new BaseResponse();
-        response.setCode(HttpStatus.OK.value());
-        response.setMessage(list.isEmpty() ? "Employment Type is Empty" : "Employment Type is Success");
-        response.setData(list);
-        return ResponseEntity.ok(response);
+    public ResponseEntity<?> getAll(){
+        return ResponseEntity.ok(BaseResponse.success(employmentTypeService.getAll(), "OK"));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<BaseResponse> getById(@PathVariable int id){
-        Optional<EmploymentType> optional = employmentTypeService.getById(id);
-        BaseResponse response = new BaseResponse();
-        if (optional.isPresent()) {
-            response.setCode(HttpStatus.OK.value());
-            response.setMessage("Get employment Type successfully");
-            response.setData(optional.get());
-            return ResponseEntity.ok(response);
-        } else {
-            response.setCode(HttpStatus.NOT_FOUND.value());
-            response.setMessage(" employment Type with id " + id + " not found");
-            response.setData(null);
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
-        }
+    public ResponseEntity<?> getById(@PathVariable int id){
+        return ResponseEntity.ok(BaseResponse.success(employmentTypeService.getById(id), "OK"));
     }
 
     @PostMapping
-    public ResponseEntity<BaseResponse> create(@RequestBody EmploymentType employmentType){
-        EmploymentType created = employmentTypeService.create(employmentType);
-        BaseResponse response = new BaseResponse(HttpStatus.CREATED.value(), "Create Employment Type successfully", created);
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    public ResponseEntity<?> create(@RequestBody EmploymentTypeRequest employmentType){
+        return ResponseEntity.ok(BaseResponse.success(employmentTypeService.create(employmentType), "Tạo EmploymentType thành công"));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<BaseResponse> update(@PathVariable int id, @RequestBody EmploymentType employmentType) {
-        EmploymentType updated = employmentTypeService.update(id, employmentType);
-        BaseResponse response = new BaseResponse();
-        if(updated != null){
-            response.setCode(HttpStatus.OK.value());
-            response.setMessage("Update Employment Type successfully");
-            response.setData(updated);
-            return ResponseEntity.ok(response);
-        } else {
-            response.setCode(HttpStatus.NOT_FOUND.value());
-            response.setMessage(" employment Type with id " + id + " not found");
-            response.setData(null);
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
-        }
+    public ResponseEntity<?> update(@PathVariable int id, @RequestBody EmploymentTypeRequest employmentType) {
+        return ResponseEntity.ok(BaseResponse.success(employmentTypeService.update(id, employmentType), "Cập nhật Joblevel thành công"));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<BaseResponse> delete(@PathVariable int id) {
-        boolean deleted = employmentTypeService.delete(id);
-        BaseResponse response = new BaseResponse();
-        if (deleted) {
-            response.setCode(HttpStatus.OK.value());
-            response.setMessage("Delete Employment Type successfully");
-            response.setData(null);
-            return ResponseEntity.ok(response);
-        } else {
-            response.setCode(HttpStatus.NOT_FOUND.value());
-            response.setMessage(" employment Type with id " + id + " not found");
-            response.setData(null);
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
-        }
+        employmentTypeService.delete(id);
+        return ResponseEntity.ok(BaseResponse.success(null, "Xóa EmploymentType có ID " + id + " thành công"));
     }
 
 }
