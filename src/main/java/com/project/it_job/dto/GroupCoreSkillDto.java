68d4b3b5a609a40277e789542dfaabf2fbfb3f9b
Manh Cuong <nguyenmanhcuong123nmc@gmail.com>
2025-11-01 22:42:59 +0700

    create group_core_skill

---
+++ src/main/java/com/project/it_job/dto/GroupCoreSkillDto.java
@@ -0,0 +1,13 @@
+package com.project.it_job.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class GroupCoreSkillDto {
+    private int id;
+    private String name;
+}
---
+++ src/main/java/com/project/it_job/repository/GroupCoreSkillRepository.java
@@ -0,0 +1,9 @@
+package com.project.it_job.repository;
+
+import com.project.it_job.entity.GroupCoreSkill;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GroupCoreSkillRepository extends JpaRepository<GroupCoreSkill, Integer> {
+}
