cf48ec2af9dd98039b10b926f9a43b524bb62c0c
Manh Cuong <nguyenmanhcuong123nmc@gmail.com>
2025-11-01 21:48:18 +0700

    create Certificate

---
+++ src/main/java/com/project/it_job/entity/Certificate.java
@@ -0,0 +1,37 @@
+package com.project.it_job.entity;
+
+import com.project.it_job.entity.auth.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "certificate")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Certificate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "certificate_name")
+    private String certificateName;
+
+    private String organization;
+
+    private LocalDate date;
+
+    private String link;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+}
