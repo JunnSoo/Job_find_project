5f99bbbd6c8ee89584e8ebcba7b3a51c0cad82ca
HuynhBao <huynhbao0626@gmail.com>
2025-11-05 04:36:33 +0700

    CRUD-Payment còn bug sẽ kiểm tra lại sauu

--- src/main/java/com/project/it_job/entity/payment/Payment.java
+++ src/main/java/com/project/it_job/entity/payment/Payment.java
@@ -16,7 +16,7 @@ import java.time.LocalDateTime;
 @Builder
 public class Payment {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Column(name = "title")
