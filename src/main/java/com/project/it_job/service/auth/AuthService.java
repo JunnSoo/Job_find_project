1d313aaba68ea83f2a09eac76db9115f8fbbd756
Phu Nguyen <trungphuntp@gmail.com>
2025-11-12 19:17:10 +0700

    feature/ block login user

--- src/main/java/com/project/it_job/service/auth/AuthService.java
+++ src/main/java/com/project/it_job/service/auth/AuthService.java
@@ -1,11 +1,9 @@
 package com.project.it_job.service.auth;
 
-import com.project.it_job.dto.auth.RegisterDTO;
 import com.project.it_job.dto.auth.TokenDTO;
-import com.project.it_job.request.auth.RegisterRequest;
+import com.project.it_job.request.auth.LoginRequest;
 
 public interface AuthService {
-    TokenDTO login(String email, String password);
-    RegisterDTO register(RegisterRequest registerRequest);
+    TokenDTO login(LoginRequest loginRequest);
     void logout(String email);
 }
