aa204bd5c004ee5c59c10367cd0ee32e841aa64b
Trung Phú <trungphuntp@gmail.com>
2025-12-04 14:55:21 +0700

    Update SecurityConfig.java

--- src/main/java/com/project/codinviec/config/SecurityConfig.java
+++ src/main/java/com/project/codinviec/config/SecurityConfig.java
@@ -90,7 +90,7 @@ public class SecurityConfig {
         @Bean
         public CorsConfigurationSource corsConfigurationSource() {
                 CorsConfiguration configuration = new CorsConfiguration();
-                configuration.setAllowedOrigins(List.of(localhost, "http://localhost:3000"));
+                configuration.setAllowedOriginPatterns(List.of(localhost, "http://localhost:3000"));
                 configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                 configuration.setAllowedHeaders(List.of("*"));
                 configuration.setAllowCredentials(true);
